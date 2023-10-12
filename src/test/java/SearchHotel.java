98d90a5df6849e4c52ff9cdb2d7356be3755f26f
bednarowicz <bednarowiczr@gmail.com>
2023-10-12 20:08:43 +0200

    wyszukanie dubaiu, pierwsze kroki

---
+++ src/test/java/SearchHotel.java
@@ -0,0 +1,23 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
+import java.util.concurrent.TimeUnit;
+
+public class SearchHotel {
+    @Test
+    public void searchHotel() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+    }
+}
