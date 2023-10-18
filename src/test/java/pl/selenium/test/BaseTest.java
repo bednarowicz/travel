0d3a3d548ea3af1e5d11a506a104f3399f5edcd0
bednarowicz <bednarowiczr@gmail.com>
2023-10-18 21:43:02 +0200

    Dodanie testng.xml, równoległe testy

--- src/test/java/pl/selenium/test/BaseTest.java
+++ src/test/java/pl/selenium/test/BaseTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
