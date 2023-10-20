6c5bc906ff7b189cdb5fc21586d40d4aab7d2b1a
bednarowicz <bednarowiczr@gmail.com>
2023-10-20 23:05:38 +0200

    adding excel reader and needed maven change

---
+++ src/test/java/pl/selenium/utils/ExcelReader.java
@@ -0,0 +1,44 @@
+package pl.selenium.utils;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExcelReader {
+    public static void readExcel(String filename) throws IOException {
+        File file = new File("src/test/resources/" + filename);
+        FileInputStream inputStream = new FileInputStream(file);
+
+        Workbook workbook = null;
+        String fileExt = filename.substring(filename.indexOf("."));
+        if(fileExt.equals(".xlsx")){
+            workbook = new XSSFWorkbook(inputStream);
+        } else if(fileExt.equals(".xls")) {
+            workbook = new HSSFWorkbook(inputStream);
+        }
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+        int rowCount = sheet.getLastRowNum();
+
+        for (int i=1; i<=rowCount; i++){
+            Row row = sheet.getRow(i);
+
+            System.out.println(row.getCell(0).getStringCellValue());
+            System.out.println(row.getCell(1).getStringCellValue());
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        readExcel("testData.xlsx");
+    }
+}
